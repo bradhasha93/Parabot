9142bf90536492b95d07ca4aaa192330b27ef4d0
Brad Hasha <bradhasha@gmail.com>
2014-02-16 02:27:14 -0600

    **

---
+++ Parabot/sources/utils/ItemManager.java
@@ -0,0 +1,31 @@
+package utils;
+
+import java.awt.event.KeyEvent;
+
+import org.parabot.environment.input.Keyboard;
+import org.rev317.api.methods.Inventory;
+import org.rev317.api.wrappers.hud.Item;
+import org.rev317.api.wrappers.hud.Tab;
+
+public class ItemManager {
+	
+	public static Item getItem(int... ids) {
+		final Item[] items = Inventory.getItems(ids);
+		return items.length > 0 ? items[0] : null;
+	}
+
+	public static boolean hasItem(int... ids) {
+		return Inventory.getItems(ids).length > 0;
+	}
+
+	public static void interactWithItem(final Item item, final String action) {
+		if (Tab.INVENTORY.isOpen()) {
+		if (item != null)
+			item.interact(action);
+		} else {
+			Keyboard.getInstance().pressKey(KeyEvent.VK_F1);
+			Keyboard.getInstance().releaseKey(KeyEvent.VK_F1);
+		}
+	}
+
+}
